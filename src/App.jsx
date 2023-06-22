94dee35242f84ac6bad11cfde7fd5c5a8ad07b40
hasanarpat <black_the_dragon@hotmail.com>
2023-06-22 20:17:21 +0300

    auth-added

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,45 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import Home from './pages/home/Home'
-import Single from './pages/single/Single'
-import Favorite from './pages/favorite/Favorite'
+import Home from "./pages/home/Home";
+import Single from "./pages/single/Single";
+import Favorite from "./pages/favorite/Favorite";
 import Navbar from "./components/navbar/Navbar";
-import './app.scss'
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import {AuthContext} from "./context/AuthContext";
+
+import "./app.scss";
+import { useContext } from "react";
 
 const App = () => {
-  return (
-    <div className="app">
-      <BrowserRouter>
-      <Navbar/>
-        <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/single/:id" element={<Single/>} />
-          <Route path="/favorite" element={<Favorite/>} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  )
-}
+	const { currentUser } = useContext(AuthContext);
 
-export default App
+	const RequireAuth = ({ children }) => {
+		return currentUser ? children : <Navigate to="/login" />;
+	};
+
+	return (
+		<div className="app">
+			<BrowserRouter>
+				<Navbar />
+				<Routes>
+					<Route path="/">
+						<Route path="login" element={<Login />} />
+						<Route path="register" element={<Register />} />
+						<Route index element={<Home />} />
+						<Route path="single/:id" element={<Single />} />
+						<Route
+							path="favorite"
+							element={
+								<RequireAuth>
+									<Favorite />
+								</RequireAuth>
+							}
+						/>
+					</Route>
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
+};
+
+export default App;
